58841115b8163611993c85f4df952dd2d2b95f6e
Billy Ceskavich <bceskavich@gmail.com>
2015-06-17 17:02:56 -0400

    Changed express server port

--- src/server.js
+++ src/server.js
@@ -11,7 +11,7 @@ app.get('/', function(req, res){
   res.sendFile(path.resolve(__dirname, './views/index.html'));
 });
 
-server = app.listen(process.env.PORT || 3000, function(){
+server = app.listen(process.env.PORT || 8080, function(){
   var port = server.address().port;
   console.log('Server is listening at %s', port);
 });
